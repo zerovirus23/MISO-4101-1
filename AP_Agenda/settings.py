73803a745b6b63ecb4468bd31834757c0e593055
Hernán Tenjo <zerovirus23@gmail.com>
2015-01-28 18:06:44 -0500

    no message

--- AP_Agenda/settings.py
+++ AP_Agenda/settings.py
@@ -86,6 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
 
 #Custome template folder
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
