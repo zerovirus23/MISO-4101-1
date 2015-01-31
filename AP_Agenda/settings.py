d3725e0bd2c26abc93d1e5d003da64e1e88e29ff
jhony <jhonyt37@gmail.com>
2015-01-31 18:11:31 -0500

    cambio en settings statics

--- AP_Agenda/settings.py
+++ AP_Agenda/settings.py
@@ -87,6 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 #Custome template folder
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
